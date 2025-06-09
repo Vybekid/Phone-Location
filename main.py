56459f87c96a6155a01cf6c4d31d55dec2db10de
Vybekid <felobitto@gmail.com>
2025-06-09 14:42:49 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import phonenumbers
-from phonenumbers import geocoder
+from phonenumbers import geocoder                                       
 phone_number1 = phonenumbers.parse("+917294536271")
 phone_number2 = phonenumbers.parse("+918878586271")
 phone_number3 = phonenumbers.parse("+12136574429")
@@ -13,3 +13,4 @@ print(geocoder.description_for_number(phone_number4, "en"));
 
 
 # LETS TRACK PHONE NUMBERS
+
